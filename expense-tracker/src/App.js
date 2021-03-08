315d1c6959bf356d621c0896f741b567ece06035
Gaurav Rawal <gauravrawal.dev@gmail.com>
2021-03-08 21:25:00 +0530

    Added Transaction list component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
---
+++ expense-tracker/src/components/TransactionList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul id="list" class="list">
+        <li class="minus">
+          Cash <span>-$400</span>
+          <button class="delete-btn">x</button>
+        </li>
+      </ul>
+    </>
+  );
+};
