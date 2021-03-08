b66afe636f9becf8bedc7e9d141b341536b362f4
Gaurav Rawal <gauravrawal.dev@gmail.com>
2021-03-08 21:13:24 +0530

    project Setup

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
+}
+
+export default App;
