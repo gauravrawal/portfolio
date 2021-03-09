8991a820870da8e7125ff415dbd23bef171ab59a
Gaurav Rawal <gauravrawal.dev@gmail.com>
2021-03-09 11:29:22 +0530

    added add transaction and delete functionality

--- expense-tracker/src/context/GlobalState.js
+++ expense-tracker/src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from "./AppReducer";
 
 // Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [{ id: 1, text: "Salary", amount: 300 }],
 };
 
 // Create context
@@ -17,8 +12,28 @@ export const GlobalContext = createContext(initialState);
 // Provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
